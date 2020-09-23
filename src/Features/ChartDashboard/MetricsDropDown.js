11dff55022d957a6801995fb657d59ab45c754f7
Shashank Gupta <shashankcm90@gmail.com>
2020-09-23 02:14:29 -0400

    Populated Data into Select dropdown using getMetrics API

--- src/Features/ChartDashboard/MetricsDropDown.js
+++ src/Features/ChartDashboard/MetricsDropDown.js
@@ -1,19 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Select from 'react-select';
 import makeAnimated from 'react-select/animated';
+import { Provider, createClient, useQuery } from 'urql';
+
+import { actions as metricActions } from './redux/reducer';
 
 const animatedComponents = makeAnimated();
 
+const client = createClient({
+  url: 'https://react.eogresources.com/graphql',
+});
+
+const MetricsQuery = `
+  query {
+    getMetrics
+  }
+`;
+
 const MetricsDropDown = () => {
+  const [result] = useQuery({
+    query: MetricsQuery,
+  });
+  const dispatch = useDispatch();
+  const [metrics, setMetrics] = useState([]);
+
+  const { data, error, fetching } = result;
+
+  useEffect(() => {
+    if (error) {
+      dispatch(metricActions.metricsApiErrorReceived({ error: error.message }));
+      return;
+    }
+    if (!data) return;
+    const { getMetrics } = data;
+
+    const listOfMetrics = getMetrics.map(metric => ({
+      label: metric,
+      value: metric,
+    }));
+
+    setMetrics(listOfMetrics);
+  }, [dispatch, data, error]);
+
+  const setSelectedMetrics = selectedMetrics =>
+    dispatch(metricActions.selectdMetricsData({ metrics: selectedMetrics }));
+
+  if (fetching) return <h1>Loading....</h1>;
+
   return (
     <Select
       closeMenuOnSelect={false}
       components={animatedComponents}
       placeholder="select metrics..."
       isMulti
-      options={[]}
+      options={metrics}
+      onChange={setSelectedMetrics}
     />
   );
 };
 
-export default MetricsDropDown;
+export default () => {
+  return (
+    <Provider value={client}>
+      <MetricsDropDown />
+    </Provider>
+  );
+};
---
+++ src/Features/ChartDashboard/redux/reducer.ts
@@ -0,0 +1,33 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+type Metric = {
+  label: string;
+  value: string;
+};
+
+export type DashboardMetrics = {
+  metrics: Metric[];
+};
+
+export type ApiErrorAction = {
+  error: string;
+};
+
+const initialState: { metrics: Metric[] } = {
+  metrics: [],
+};
+
+const slice = createSlice({
+  name: 'metrics',
+  initialState,
+  reducers: {
+    selectdMetricsData: (state, action: PayloadAction<DashboardMetrics>) => {
+      const { metrics } = action.payload;
+      state.metrics = metrics;
+    },
+    metricsApiErrorReceived: (state, action: PayloadAction<ApiErrorAction>) => state,
+  },
+});
+
+export const reducer = slice.reducer;
+export const actions = slice.actions;
